424961eaa6bb64fdac58ee75175ed4e771b74e81
student <moskvittina17@mail.ru>
2023-03-02 18:20:18 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+def proc1():
+    slovo = input()
+    x = ''
+    while slovo != "stop":
+            x = x + slovo + ' '
+            slovo = input()
+    print(x)
+
+def proc4():
+    from random import randint
+    m = 0
+    n = 0
+    while m < 3:
+        a = randint(1,10)
+        b = randint(1, 10)
+        itog = int(input(str(a) + "+" + str(b) + "="))
+        if a + b != itog:
+                print("Не верно :(")
+                m+=1
+        else:
+                print("Верно :)")
+                n+=1
+    if m == 3:
+        print("Game over! Правильных ответов:", n)
+
+
+
+
+
+
+
+def proc3():
+    slovo = input()
+    if 'ф' or 'Ф' in slovo:
+        print("Ого! Это редкое слово!")
+    else:
+        print("Эх, это не очень редкое слово!")
+proc1(), proc4(), proc3()
